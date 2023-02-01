625dc38b0ecca3ac6499b3c1f276371a0323f28e
SurenOhanesian <115486102+SurenOhanesian@users.noreply.github.com>
2023-02-01 14:43:27 +0200

    init binding, homefragment

---
+++ app/src/main/java/com/example/redditandroid/ui/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.redditandroid.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.redditandroid.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (_binding == null) _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
